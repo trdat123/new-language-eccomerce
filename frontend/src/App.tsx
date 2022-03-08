ca4df8692ee6233a355b255c5c0b0e13500f88d8
lequocduy18 <68944776+lequocduy18@users.noreply.github.com>
2022-03-08 10:25:02 +0700

    test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React + Le Quoc Duy !</p>
+        <p>Le Quoc Duy !</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
