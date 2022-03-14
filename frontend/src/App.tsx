a7a4f0548525c67d844ac67d76d2e117069ba32f
lequocduy18 <68944776+lequocduy18@users.noreply.github.com>
2022-03-14 08:13:44 +0700

    connect backend to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,21 @@
-import { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import Card1 from "./Card";
+import React from "react";
+import Container from "@mui/material/Container";
+import ProductCard from "./components/ProductCard";
+import NavigationMenu from "./components/NavigationMenu";
+import Footer from "./components/Footer"
+import ButtonAppBar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Le Quoc Duy 1232!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
+    <React.Fragment>
+      <NavigationMenu />
+      <Container>
+        <ProductCard />
+      </Container>
 
-          <Card1></Card1>
-        </p>
-      </header>
-    </div>
+      <Footer />
+    </React.Fragment>
   );
 }
 
