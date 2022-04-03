7df1841c5af7270c0cd05df27b2faa2618e04641
trdat123 <datngt1111@gmail.com>
2022-04-03 15:57:11 +0700

    fix vercel deployment bug 2

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ let bodyParser = require("body-parser");
 require("dotenv").config();
 
 // Express Route
-const movieRoute = require("./api/movie.route");
+const movieRoute = require("./movie.route");
 
 // Configure mongoDB Database
 // mongoose.set("useNewUrlParser", true);
